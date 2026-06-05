42752bf736adc78c1780e4fb29f50b4f2e1fc326
𝑲𝒂𝒛𝒊 龍￤ 🃏🥃 <kazinhuhgmrs@gmail.com>
2026-06-05 10:41:46 -0300

    Delete users/js directory

--- users/js/themes.js
+++
@@ -1,5 +0,0 @@
-const THEME_STORAGE_KEY = 'vl-theme';
-const AVAILABLE_THEMES = ['default','soft','fantasy','dark'];
-function applyTheme(themeName = 'default'){ const theme = AVAILABLE_THEMES.includes(themeName) ? themeName : 'default'; document.documentElement.dataset.theme = theme; localStorage.setItem(THEME_STORAGE_KEY, theme); return theme; }
-function getSavedTheme(){ return localStorage.getItem(THEME_STORAGE_KEY) || 'default'; }
-function initTheme(){ applyTheme(getSavedTheme()); }
