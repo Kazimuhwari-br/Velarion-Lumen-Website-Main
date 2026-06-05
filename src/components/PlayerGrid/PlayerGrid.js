c725c9d76e6dd22dff87299e995f5a4580e3b926
𝑲𝒂𝒛𝒊 龍￤ 🃏🥃 <kazinhuhgmrs@gmail.com>
2026-06-05 11:20:56 -0300

    Delete src directory

--- src/components/PlayerGrid/PlayerGrid.js
+++
@@ -1,9 +0,0 @@
-import { renderPlayerCard } from '../PlayerCard/PlayerCard.js';
-
-export function renderPlayerGrid(players) {
-  if (!players.length) {
-    return '<p class="empty-state">Nenhum jogador encontrado.</p>';
-  }
-
-  return `<section class="player-grid">${players.map(renderPlayerCard).join('')}</section>`;
-}
