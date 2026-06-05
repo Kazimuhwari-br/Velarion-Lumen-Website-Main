6759e2baff1f12a50972b144df18dbba0c59989a
𝑲𝒂𝒛𝒊 龍￤ 🃏🥃 <kazinhuhgmrs@gmail.com>
2026-06-05 11:29:29 -0300

    Delete src directory

--- src/services/http.service.js
+++
@@ -1,9 +0,0 @@
-export async function fetchJson(url) {
-  const response = await fetch(url, { headers: { Accept: 'application/json' } });
-
-  if (!response.ok) {
-    throw new Error(`Falha ao buscar ${url}: ${response.status}`);
-  }
-
-  return response.json();
-}
